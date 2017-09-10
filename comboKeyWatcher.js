3b0abf31a00cbf79339b0777cd5f8d9a1685cf10
Clowst <bon.kevin@gmail.com>
2017-09-10 11:55:31 -0400

    ComboKeyWatcher

--- comboKeyWatcher.js
+++ comboKeyWatcher.js
@@ -1,24 +1,22 @@
-
-const konamiCode = [
-  '38', // Up
-  '38', // Up
-  '40', // Down
-  '40', // Down
-  '37', // Left
-  '39', // Right
-  '37', // Left
-  '39', // Right
-  '66', // B
-  '65', // A
-  // '13', // Enter
-];
-
-var comboKeyWatched = function(_window) {
-  function public()
+var comboKeyWatcher = function(_window) {
+  function public() {}
   
   let isWatching = false;
   let registered = new Map;
   let keysRegistered = new Map;
+  
+  function stopEventListener () {
+    isWatching = false;
+    _window.removeEventListener('keydown', onKeyDown);
+    return this;
+  }
+
+  function initEventListener () {
+    stopEventListener();
+    isWatching = true;
+    _window.addEventListener('keydown', onKeyDown); // Instead of keyup to handle combo key like: ctrl + a
+    return this;
+  }
 
   function resetAllCurrentIndex(skips = []) {
     const skippedHash = skips.reduce((memo, name) => {
@@ -32,21 +30,23 @@ var comboKeyWatched = function(_window) {
     return this;
   }
 
-  function onKeyUp(e) {
+  function onKeyDown(e) {
     let skippable = [];
-
-    if (!e.keyCode in keysRegistered) {
+    let keyCode = parseInt(e.keyCode, 10);
+    if (!keysRegistered.has(keyCode)) { // None watcher have this key on watch
       return;
     } else {
-      keysRegistered[e.keyCode].forEach(elem => {
-        elem.refObj.currentIndex +=1;
-        // Right order
-        if (elem.index === elem.refObj.currentIndex) {
-          if (elem.refObj.currentIndex && elem.refObj.maxIndex) {
-            elem.refObj.callback();
+      let alreadyHandled = {};
+      keysRegistered.get(keyCode).forEach(elem => {
+        // Right order?
+        if (!(elem.refObj.name in alreadyHandled) && elem.position === elem.refObj.currentIndex) {
+          if (elem.refObj.currentIndex === elem.refObj.maxIndex) { // Sequence finished
+            elem.refObj.callback.call(public);
           } else {
             skippable.push(elem.refObj.name);
+            elem.refObj.currentIndex +=1;
           }
+          alreadyHandled[elem.refObj.name] = true;
         }
       });
     }
@@ -54,48 +54,193 @@ var comboKeyWatched = function(_window) {
     return this;
   }
 
-  initEventListener = () => {
-    isWatching = true;
-    window.addEventListener('keyup', onKeyUp);
-    return this;
+  function reformatKey (key) {
+    if (typeof key === 'number') return key;
+    key = String(key).toLowerCase();
+    if (!(key in result.keys)) {
+      throw Error(`Key ${key} not found. Take a look at the available keys with comboKeyWatcher.keys`);
+    }
+    return result.keys[key];
   }
 
-  register = (keys = [], name = null, callback = function() {}) => {
+  function reformatKeys (keys) {
+    return keys.map(reformatKey);
+  }
+  
+  function register (keys = [], name = null, callback = function() {}) {
+    if (registered.has(name)) {
+      throw new Error(`name ${name} already registered, pick another`);
+    }
     if (!isWatching) {
       initEventListener();
     }
-    // TODO: Make it compatible with no name or non unique
-    // if (name == null) {
-      
-    // }
+
+    keys = reformatKeys(keys);
     let refObj = {
-      _uid: name,
       name,
       currentIndex: 0,
-      maxIndex: keys.length,
+      maxIndex: keys.length - 1,
       keys,
       callback
     };
     registered.set(name, refObj);
-    for (let [index, key] of keys.entries()) {
+    for (let [position, key] of keys.entries()) {
       if (!keysRegistered.has(key)) {
         keysRegistered.set(key, []);
       }
-      keysRegistered.set(key, keysRegistered.get(key).push({
-        index,
-        refObj
-      }));
+      let keyRegister = keysRegistered.get(key);
+      keyRegister.push({ position, refObj });
+      keysRegistered.set(key, keyRegister);
     }
     return this;
   }
 
-  public.prototype.watch = (keys = [], name = null, callback = function() {}) => {
-    register(keys, callback);
+  function unregister(name) {
+    if (!registered.has(name)) return this;
+    let target = registered.get(name);
+    for (let key of target.keys) {
+      if (!keysRegistered.has(key)) {
+        continue;
+      };
+      let keysRegisteredTarget = keysRegistered.get(key);
+      if (keysRegisteredTarget.length === 1) {
+        keysRegistered.delete(key);
+      } else {
+        for (let [index, keysRegisteredTargetElement] of keysRegisteredTarget.entries()) {
+          if (keysRegisteredTargetElement.refObj.name === name) {
+            keysRegisteredTarget.splice(index, 1);
+            break;
+          }
+        }
+      }
+    }
+    registered.delete(name);
+    if (registered.size === 0)
+      stopEventListener();
+    return this;
+  }
+
+  function getKeys() {
+    // Based on: https://github.com/timoxley/keycode
+    let codes = {
+      'windows': 91,
+      '⇧': 16,
+      '⌥': 18,
+      '⌃': 17,
+      '⌘': 91,
+      'control': 17,
+      'ctl': 17,
+      'ctrl': 17,
+      'option': 18,
+      'pause': 19,
+      'break': 19,
+      'caps': 20,
+      'return': 13,
+      'escape': 27,
+      'esc': 27,
+      'spc': 32,
+      'insert': 45,
+      'ins': 45,
+      'delete': 46,
+      'del': 46,
+      'command': 91,
+      'cmd': 91,
+      'backspace': 8,
+      'tab': 9,
+      'enter': 13,
+      'shift': 16,
+      'alt': 18,
+      'pause/break': 19,
+      'caps lock': 20,
+      'space': 32,
+      'page up': 33,
+      'pgup': 33,
+      'page down': 34,
+      'pgdn': 34,
+      'end': 35,
+      'home': 36,
+      'left': 37,
+      'up': 38,
+      'right': 39,
+      'down': 40,
+      'left command': 91,
+      'right command': 93,
+      'numpad *': 106,
+      'numpad +': 107,
+      'numpad -': 109,
+      'numpad .': 110,
+      'numpad /': 111,
+      'num lock': 144,
+      'scroll lock': 145,
+      'my computer': 182,
+      'my calculator': 183,
+      ';': 186,
+      '=': 187,
+      ',': 188,
+      '-': 189,
+      '.': 190,
+      '/': 191,
+      '`': 192,
+      '[': 219,
+      '\\': 220,
+      ']': 221,
+      '\'': 222
+    };
+
+    // lower case chars
+    for (i = 97; i < 123; i++) codes[String.fromCharCode(i)] = i - 32
+  
+    // numbers
+    for (var i = 48; i < 58; i++) codes[i - 48] = i
+  
+    // function keys
+    for (i = 1; i < 13; i++) codes['f'+i] = i + 111
+  
+    // numpad keys
+    for (i = 0; i < 10; i++) codes['numpad '+i] = i + 96
+    
+    return codes;
+  }
+
+  public.prototype.watch = function (keys = [], name = null, callback = function() {}) {
+    register(keys, name, callback);
     return this;
   };
-  public.prototype.stopWatch = (name = null) => {
+  public.prototype.stopWatch = function (name = null) {
+    unregister(name);
     return this;
   };
 
-  return new public;
+  let result = new public;
+  // Helper aliases
+  result.keys = getKeys();
+  return result;
 }(window);
+
+var konamiCode = [
+  'Up',
+  'Up',
+  'Down',
+  'Down',
+  'Left',
+  'Right',
+  'Left',
+  'Right',
+  'B',
+  'A',
+];
+
+comboKeyWatcher
+  // KonamiCode
+  .watch(konamiCode, 'konamiCode', function() {
+    console.log('Time to implement something crazy!');
+  })
+  // With keyCode number
+  .watch([17, 65], 'all', function() {
+    console.log('all');
+  })
+  // With String (will only be triggered once)
+  .watch(['Ctrl', 'a'], 'allText', function() {
+    console.log('allText');
+    comboKeyWatcher.stopWatch('allText');
+  });
